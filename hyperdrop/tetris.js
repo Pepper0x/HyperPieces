5884b9ade06caccd2e7c0eb59f3193958873d3ca
Pepper0x <pepperfromx@gmail.com>
2025-05-17 15:55:21 -0700

    Add files via upload

--- hyperdrop/tetris.js
+++ hyperdrop/tetris.js
@@ -1,1 +1,14 @@
-// Modified tetris.js with mobile slide support
+
+function startGame() {
+  document.getElementById('start-screen').style.display = 'none';
+}
+
+function showLeaderboard() {
+  document.getElementById('start-screen').style.display = 'none';
+  document.getElementById('leaderboard-screen').style.display = 'flex';
+}
+
+function goToMenu() {
+  document.getElementById('leaderboard-screen').style.display = 'none';
+  document.getElementById('start-screen').style.display = 'flex';
+}
